987a5176e15fea5fffed90ec52b1d521795ab1b6
Yawndu <xadrianpinera@gmail.com>
2025-03-11 10:11:31 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Mlab2_Piñera
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ItemsListView.ItemsSource = Items;
+        }
+        private void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            Items.Add(ItemEntry.Text);
+            ItemEntry.Text = string.Empty;
+        }
+
+
+
+    }
+
+    
+
+}
